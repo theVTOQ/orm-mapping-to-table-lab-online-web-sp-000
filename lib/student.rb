f84ab43d7416e57baffc63df934f191ab8b315c2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:11:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
       VALUES (?, ?);
     SQL
     DB[:conn].execute(sql, @name, @grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs;")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
   end
 
   def self.create(name:, grade:)
